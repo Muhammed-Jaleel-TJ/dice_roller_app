0b58fb97b661555bc6c2e6555ff39151a7bd86e0
Muhammed Jaleel TJ <138207941+Muhammed-Jaleel-TJ@users.noreply.github.com>
2024-09-29 19:27:42 +0530

    first commint

---
+++ lib/dice_roller.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var randoMizer = Random();
+  var currentDiceRoll = 3;
+
+  void rollDice() {
+    setState(() {
+      currentDiceRoll = randoMizer.nextInt(6) + 1; // >0 <7
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$currentDiceRoll.png',
+          width: 250,
+        ),
+        const SizedBox(height: 20),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text('Roll Dice'),
+        ),
+      ],
+    );
+  }
+}
